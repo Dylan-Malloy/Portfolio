cc3557a9141e259c0b41b39823314f2c232cbf34
Dylan-Malloy <dylantmalloy1@gmail.com>
2024-12-23 16:13:55 -0500

    greaterTen function added

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ def is_odd(num : int) -> bool:
     if num % 2 == 1:
         return True
     return False
+
+def greaterten(num : int) -> bool:
+    if num > 10:
+        return True
+    return False
