34751b15b4d865e01a8b5190c92764006f5cd53a
RyanMalloy <129435196+RyanMalloy@users.noreply.github.com>
2024-12-24 13:41:54 -0500

    sectoins created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import "./styles/App.css";
 function App() {
   return (
     <>
       <section id="hero">
         <h1>Hero</h1>
+      </section>
+
+      <section id="about">
+        <h1>About</h1>
+      </section>
+
+      <section id="projects">
+        <h1>Projects</h1>
+      </section>
+
+      <section id="contact">
+        <h1>Contact</h1>
       </section>
     </>
   );
