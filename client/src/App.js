e6d6db15eeaa6ecf296746fa48bf9b4647035b67
Dylan-Malloy <dylantmalloy1@gmail.com>
2024-12-24 13:39:16 -0500

    added navbar to website

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+import "./styles/App.css"
 function App() {
   return (
     <>
+      <Navbar/>
       <section id="hero">
         <h1>Hero</h1>
       </section>
